ffb4272ea44a48b30e199e3facb009224e817f47
Sebastian Gonczarek <sebastian.gonczarek@gmail.com>
2020-11-03 12:07:18 +0100

    moved enum to Porgrammzustaende.cs

---
+++ Programmzustaende.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    enum Programmzustaende
+    {
+        Stopping,
+        Paused,
+        Starting,
+        Stopped
+    }
+    
+    
+    
+}
